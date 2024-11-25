e20011029727795dc2eefa326db6fc34c8216f82
Andrew <t8mj75@gmail.com>
2024-11-25 17:20:13 +1100

    Ruby on rails setup with model, view and controllers for Article and comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def create
+    # Can use a gem (slug?) to replace number id with string
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+    # Email github profile / repository to Michael to be added to Slack + Code reviews
+  end
+
+  private
+  def comment_params
+    params.expect(comment: [ :commenter, :body, :status ])
+  end
+end
