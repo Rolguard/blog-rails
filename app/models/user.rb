f7eab63fa7d5d378cd2eb640ed865760e0d04be1
Andrew <t8mj75@gmail.com>
2024-12-06 17:05:35 +1100

    Added username column to User model and error validation in log in page (devise/sessions/new.html.erb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: true
+
+  def email_required?
+    false
+  end
 end
