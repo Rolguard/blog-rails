645d6aba6be36e90725f6899adcf8757481d4ddf
Andrew <t8mj75@gmail.com>
2024-12-20 16:18:46 +1100

    Articles that are viewable on a user profile must have a publish_status of 'public' and approval_status of 'accepted' unless they are viewed by an admin or the user themselves. Added message for users to indicate their article was accepted by an admin.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,13 @@
 module ArticlesHelper
-  def public_and_accepted(articles)
+  def public_and_accepted_article(article)
+    article.publish_status == "public" && article.approval_status == "accepted"
+  end
+
+  def any_public_and_accepted(articles)
     articles.any? { |article| article.publish_status == "public" && article.approval_status == "accepted" }
+  end
+
+  def public_and_accepted_count(articles)
+    articles.where(publish_status: "public", approval_status: :accepted).count
   end
 end
