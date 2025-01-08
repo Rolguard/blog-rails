adc1ec68bfc40dea9ca943d994702c3527a3037a
Andrew <t8mj75@gmail.com>
2025-01-08 16:13:55 +1100

    Added minimum height to rich text editor. Added word counter to ActionText rich text editor but still bug where word counter does not work on initial page load, only after refresh or redirect to another page.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,37 +5,32 @@ import '@popperjs/core'
 import "bootstrap"
 import "trix"
 import "@rails/actiontext"
+// import "wordCounter"
 
-window.addEventListener("trix-file-accept", function(event) {
-  event.preventDefault()
-  alert("File attachment not supported!")
-});
+// window.addEventListener("trix-file-accept", function(event) {
+//   event.preventDefault()
+//   alert("File attachment not supported!")
+// });
 
-document.addEventListener("turbo:load", () => {
-  var tagInput = document.querySelector('input[name="input-custom-dropdown"]');
-  if (!tagInput) {
-    console.warn("[Tagify]: Input element not found in DOM.");
-    return;
-  }
+// document.addEventListener("turbo:load", () => {
+//   var tagInput = document.querySelector('input[name="input-custom-dropdown"]');
+//   if (tagInput) {
+//     var tagWhiteList = JSON.parse(tagInput.getAttribute("data-whitelist"))
+//     // var tagWhiteList = tagInput.getAttribute("data-whitelist")
+//     console.log(typeof tagWhiteList)
+
+
+//     console.log(tagWhiteList)
+//     console.log(Array.isArray(tagWhiteList))
   
-  console.log("Tagify input element found:", tagInput);
-  if (tagInput) {
-    var tagWhiteList = JSON.parse(tagInput.getAttribute("data-whitelist"))
-    // var tagWhiteList = tagInput.getAttribute("data-whitelist")
-    console.log(typeof tagWhiteList)
-
-
-    console.log(tagWhiteList)
-    console.log(Array.isArray(tagWhiteList))
-  
-    var tagify = new Tagify(tagInput, {
-        // originalInputValueFormat: tagWhiteList => tagWhiteList.map(item => item.value).join(','),
-        whitelist: tagWhiteList,
-        dropdown: {
-            classname: 'tags-look', // <- custom classname for this dropdown, so it could be targeted
-            enabled: 0,             // <- show suggestions on focus
-            closeOnSelect: false,    // <- do not hide the suggestions dropdown once an item has been selected
-        }
-    })
-  }
-})
+//     var tagify = new Tagify(tagInput, {
+//         // originalInputValueFormat: tagWhiteList => tagWhiteList.map(item => item.value).join(','),
+//         whitelist: tagWhiteList,
+//         dropdown: {
+//             classname: 'tags-look', // <- custom classname for this dropdown, so it could be targeted
+//             enabled: 0,             // <- show suggestions on focus
+//             closeOnSelect: false,    // <- do not hide the suggestions dropdown once an item has been selected
+//         }
+//     })
+//   }
+// })
